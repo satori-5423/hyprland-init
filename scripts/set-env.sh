6d2d3636ff49c7c0c10c0b373e0a959c17d4cf81
satori-5423 <suxue2746841150@gmail.com>
2025-11-16 14:38:44 +0800

    Update set-env.sh

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 
-sudo cp -v ./files/pacman/mirrorlist /etc/pacman.d/mirrorlist
-sudo cp -v ./files/pacman/pacman.conf /etc/pacman.conf
-sudo pacman -Syyuu --noconfirm
-
 INIT_PATH=$(pwd)
 
-git clone https://github.com/end-4/dots-hyprland.git
-cd "$INIT_PATH/dots-hyprland/"
-chmod +x ./setup
-./setup install
+sudo cp -v "$INIT_PATH/files/pacman/mirrorlist /etc/pacman.d/mirrorlist"
+sudo cp -v "$INIT_PATH/files/pacman/pacman.conf /etc/pacman.conf"
+sudo pacman -Syyuu --noconfirm
+
+git clone https://github.com/end-4/dots-hyprland.git "$INIT_PATH"
+cd "$INIT_PATH/dots-hyprland/" && chmod +x ./setup && ./setup install
+
+tar -xzvf "$INIT_PATH/files/grub/Inoue-Takina.tar.gz" --directory "$INIT_PATH"
+cd "$INIT_PATH/Inoue Takina/" && chmod +x ./install.sh && sudo ./install.sh
+
+rm -rfv "$INIT_PATH/dots-hyprland/" "$INIT_PATH/Inoue Takina/"
 cd "$INIT_PATH"
 
-tar -xzvf ./files/grub/Inoue-Takina.tar.gz --directory "$INIT_PATH"
-cd "$INIT_PATH/Inoue Takina/"
-chmod +x ./install.sh
-sudo ./install.sh
-cd "$INIT_PATH"
-
-rm -rfv "$INIT_PATH/Inoue Takina/"
-rm -rfv "$INIT_PATH/dots-hyprland/"
-
-yay -Syu paru
+yay -Syu --noconfirm paru
