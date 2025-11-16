c9e067cddb94ad203aee3c46546a37fb70d467c0
satori-5423 <suxue2746841150@gmail.com>
2025-11-16 16:52:21 +0800

    Update set-env.sh

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
 INIT_PATH=$(pwd)
+CACHE_PATH="~/.cache/hyprland-init"
+
+mkdir -p "$CACHE_PATH"
 
 sudo cp -v "$INIT_PATH/files/pacman/mirrorlist" /etc/pacman.d/mirrorlist
 sudo cp -v "$INIT_PATH/files/pacman/pacman.conf" /etc/pacman.conf
 sudo pacman -Syyuu --noconfirm
 
-git clone https://github.com/end-4/dots-hyprland.git "$INIT_PATH"
-cd "$INIT_PATH/dots-hyprland/" && chmod +x ./setup && ./setup install
+if [[ -d "$CACHE_PATH/dots-hyprland/.git/" ]]; then
+    cd "$CACHE_PATH/dots-hyprland/"
+    git pull
+else
+    git clone https://github.com/end-4/dots-hyprland.git "$CACHE_PATH/dots-hyprland/"
+fi
+cd "$CACHE_PATH/dots-hyprland/" && chmod +x ./setup && ./setup install
 
-tar -xzvf "$INIT_PATH/files/grub/Inoue-Takina.tar.gz" --directory "$INIT_PATH"
-cd "$INIT_PATH/Inoue Takina/" && chmod +x ./install.sh && sudo ./install.sh
+if [[ -d "$CACHE_PATH/Inoue Takina/" ]]; then
+    rm -rf "$CACHE_PATH/Inoue Takina/"
+else
+    tar -xzvf "$INIT_PATH/files/grub/Inoue-Takina.tar.gz" --directory "$CACHE_PATH"
+fi
+cd "$CACHE_PATH/Inoue Takina/" && chmod +x ./install.sh && sudo ./install.sh
 
-rm -rfv "$INIT_PATH/dots-hyprland/" "$INIT_PATH/Inoue Takina/"
 cd "$INIT_PATH"
-
-yay -Syu --noconfirm paru
+yay -Syu --needed --noconfirm paru
