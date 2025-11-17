8859a4a46a0f7b95ae3d1a7ba105ceffedc2990a
satori-5423 <suxue2746841150@gmail.com>
2025-11-17 20:11:44 +0800

    Update perl

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -7,28 +7,7 @@
 KEYBINDS="$HOME/.config/hypr/hyprland/keybinds.conf"
 EXECS="$HOME/.config/hypr/hyprland/execs.conf"
 
-perl -i -pe '
-    if (/exec-once\s*=\s*hyprctl\s+setcursor/) {
-        s/Bibata-Modern-Classic/Bibata-Modern-Ice/;
-    }
-' "$EXECS"
-
-perl -i -pe '
-    if (/bind\s*=\s*Super,\s*E,.*launch_first_available\.sh/) {
-        if (/"dolphin"\s+"nautilus"/) {
-            s/"dolphin"/"__TEMP__"/;
-            s/"nautilus"/"dolphin"/;
-            s/"__TEMP__"/"nautilus"/;
-        }
-    }
-' "$KEYBINDS"
-
-perl -i -pe '
-    if (/bind\s*=\s*Super,\s*X,.*launch_first_available\.sh/) {
-        if (!/"zeditor"/) {
-            s/(launch_first_available\.sh\s*)/$1 . "\"zeditor\" " /e;
-        }
-    }
-' "$KEYBINDS"
+perl ./modify_configs.pl "$EXECS"
+perl ./modify_configs.pl "$KEYBINDS"
 
 cp -v ./files/hyprland/* ~/.config/hypr/custom/
