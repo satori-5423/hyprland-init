fa2580042cdff893898d83aea9b1c59394502876
satori-5423 <suxue2746841150@gmail.com>
2025-11-17 12:25:48 +0800

    nvim

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,8 +4,8 @@ chsh -s $(which fish)
 
 mkdir -p ~/.config/
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rfv ~/.config/nvim/.git
+# git clone https://github.com/LazyVim/starter ~/.config/nvim
+# rm -rfv ~/.config/nvim/.git
 
 cp -rv ./files/.config/* ~/.config/
 cp -v ./files/dotfiles/.* ~/
