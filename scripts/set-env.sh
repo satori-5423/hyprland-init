4adb66c4144e8e299fb2ed85d778cb5e930c8e9c
satori-5423 <suxue2746841150@gmail.com>
2025-11-17 12:33:44 +0800

    Update

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -10,18 +10,17 @@ sudo cp -v "$INIT_PATH/files/pacman/pacman.conf" /etc/pacman.conf
 sudo pacman -Syyuu --noconfirm
 
 if [[ -d "$CACHE_PATH/dots-hyprland/.git/" ]]; then
-    cd "$CACHE_PATH/dots-hyprland/"
-    git pull
+  cd "$CACHE_PATH/dots-hyprland/"
+  git pull
 else
-    git clone https://github.com/end-4/dots-hyprland.git "$CACHE_PATH/dots-hyprland/"
+  git clone https://github.com/end-4/dots-hyprland.git "$CACHE_PATH/dots-hyprland/"
 fi
 cd "$CACHE_PATH/dots-hyprland/" && chmod +x ./setup && ./setup install
 
 if [[ -d "$CACHE_PATH/Inoue Takina/" ]]; then
-    rm -rf "$CACHE_PATH/Inoue Takina/"
-else
-    tar -xzvf "$INIT_PATH/files/grub/Inoue-Takina.tar.gz" --directory "$CACHE_PATH"
+  rm -rf "$CACHE_PATH/Inoue Takina/"
 fi
+tar -xzvf "$INIT_PATH/files/grub/Inoue-Takina.tar.gz" --directory "$CACHE_PATH"
 cd "$CACHE_PATH/Inoue Takina/" && chmod +x ./install.sh && sudo ./install.sh
 
 cd "$INIT_PATH"
