a2824c5cc5436f78e0f42809ac627612c6d84182
satori-5423 <suxue2746841150@gmail.com>
2025-11-20 09:17:35 +0800

    Update

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -4,10 +4,4 @@
 # ~/.config/fish/config.fish
 # ~/.config/kitty/kitty.conf
 
-KEYBINDS="$HOME/.config/hypr/hyprland/keybinds.conf"
-EXECS="$HOME/.config/hypr/hyprland/execs.conf"
-
-perl ./modify_configs.pl "$EXECS"
-perl ./modify_configs.pl "$KEYBINDS"
-
 cp -v ./files/hyprland/* ~/.config/hypr/custom/
