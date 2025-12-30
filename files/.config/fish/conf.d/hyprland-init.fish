0a1c6591f101f2251a53c5f840c1fea7f110040f
satori-5423 <suxue2746841150@gmail.com>
2025-12-30 17:30:52 +0800

    Update

--- files/.config/fish/conf.d/hyprland-init.fish
+++ files/.config/fish/conf.d/hyprland-init.fish
@@ -2,4 +2,12 @@
 contains ~/.local/bin $fish_user_paths
 or set -Ua fish_user_paths ~/.local/bin
 
-source ~/.config/fish/auto-Hypr.fish
+alias vi nvim
+alias vim nvim
+
+# Auto start Hyprland on tty1
+if test -z "$DISPLAY"; and test "$XDG_VTNR" -eq 1
+    exec uwsm start hyprland.desktop
+end
+
+# source ~/.config/fish/auto-Hypr.fish
