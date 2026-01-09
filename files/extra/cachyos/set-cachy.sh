36190d35897959226477b5b711194cbd871ae1ea
satori-5423 <suxue2746841150@gmail.com>
2026-01-09 15:47:52 +0800

    Update

--- files/extra/cachyos/set-cachy.sh
+++ files/extra/cachyos/set-cachy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # Set up a temporary directory for build operations
 WORK_DIR=$(mktemp -d)
 # Get the absolute path of the script directory to find local assets (mirrors)
@@ -27,9 +26,9 @@ sudo ./cachyos-repo.sh
 
 # Move mirrorlists
 echo "Installing mirrorlists from $SCRIPT_DIR/mirrors..."
-sudo cp "$SCRIPT_DIR/mirrors/cachyos-mirrorlist" /etc/pacman.d/cachyos-mirrorlist
-sudo cp "$SCRIPT_DIR/mirrors/cachyos-v3-mirrorlist" /etc/pacman.d/cachyos-v3-mirrorlist
-sudo cp "$SCRIPT_DIR/mirrors/cachyos-v4-mirrorlist" /etc/pacman.d/cachyos-v4-mirrorlist
+sudo cp --verbose "$SCRIPT_DIR/mirrors/cachyos-mirrorlist" /etc/pacman.d/cachyos-mirrorlist
+sudo cp --verbose "$SCRIPT_DIR/mirrors/cachyos-v3-mirrorlist" /etc/pacman.d/cachyos-v3-mirrorlist
+sudo cp --verbose "$SCRIPT_DIR/mirrors/cachyos-v4-mirrorlist" /etc/pacman.d/cachyos-v4-mirrorlist
 
 # Install packages
 echo "Installing kernel and tools..."
@@ -37,3 +36,4 @@ sudo pacman -Syyu --noconfirm linux-cachyos linux-cachyos-headers cachyos-settin
 sudo systemctl enable --now ananicy-cpp
 
 # GRUB_TOP_LEVEL="/boot/vmlinuz-linux-cachyos"
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
