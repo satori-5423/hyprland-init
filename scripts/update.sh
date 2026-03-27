ec706abb01be42577ab5169de60f6a3af93d8856
satori-5423 <suxue2746841150@gmail.com>
2026-03-27 23:20:41 +0800

    something update

---
+++ scripts/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# The parent directory of scripts/ is the root of the repo
+REPO_ROOT="$(dirname "$SCRIPT_DIR")"
+
+# Exit on error
+set -e
+
+sed -i '1i # vim:ft=kitty\nbackground_opacity 0.8\n' ~/.config/kitty/kitty.conf
+sed -i 's/font_size 11.0/font_size 20.0/g' ~/.config/kitty/kitty.conf
+
+cp -v "$REPO_ROOT/configs/.config/fontconfig/fonts.conf" ~/.config/fontconfig/fonts.conf
+fc-cache -fv
+
+cp -v ~/ii-original-dots-backup/.config/dolphinrc ~/.config/dolphinrc
