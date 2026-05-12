ee44eadd736356fca271b6a6c832798165e3ac50
satori-5423 <suxue2746841150@gmail.com>
2026-05-12 17:40:04 +0800

    something update

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -49,10 +49,10 @@ fi
 
 if [[ -d "$CACHE_PATH/dots-hyprland/.git/" ]]; then
     cd "$CACHE_PATH/dots-hyprland/"
-    git stash && git pull
+    git fetch origin --depth=1 && git reset --hard origin/main
 else
     # git clone https://github.com/end-4/dots-hyprland.git "$CACHE_PATH/dots-hyprland/"
-    git clone https://github.com/satori-5423/dots-hyprland.git "$CACHE_PATH/dots-hyprland/"
+    git clone https://github.com/satori-5423/dots-hyprland.git --depth=1 "$CACHE_PATH/dots-hyprland/"
 fi
 { cd "$CACHE_PATH/dots-hyprland/" && chmod +x ./setup && ./setup install; } || true
 
