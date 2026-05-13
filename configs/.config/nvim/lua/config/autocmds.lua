28d030cf8cd35db2b8647e1d36d519a15aca4765
satori-5423 <suxue2746841150@gmail.com>
2026-05-13 13:24:21 +0800

    nvim: fcitx5 optimization

--- configs/.config/nvim/lua/config/autocmds.lua
+++ configs/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,32 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local fc_augroup = vim.api.nvim_create_augroup("FcitxGroup", { clear = true })
+
+-- local auto_restore_im = true 
+-- local saved_im = nil
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = fc_augroup,
+  desc = "Switch to US layout on InsertLeave",
+  callback = function()
+    -- if auto_restore_im then
+    --   local obj = vim.system({ "fcitx5-remote", "-n" }, { text = true }):wait()
+    --   if obj.code == 0 then
+    --     saved_im = vim.trim(obj.stdout)
+    --   end
+    -- end
+    vim.system({ "fcitx5-remote", "-s", "keyboard-us" })
+  end,
+})
+
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+--   group = fc_augroup,
+--   desc = "Restore previous IM on InsertEnter",
+--   callback = function()
+--     if auto_restore_im and saved_im and saved_im ~= "" then
+--       vim.system({ "fcitx5-remote", "-s", saved_im })
+--     end
+--   end,
+-- })
