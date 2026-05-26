5759d0f0defc47a1a5c5e67d6628ac6ce39ecc30
satori-5423 <suxue2746841150@gmail.com>
2026-05-26 17:53:15 +0800

    custom kitty config

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,6 +10,7 @@ set -e
 
 sed -i '1i # vim:ft=kitty\nbackground_opacity 0.8\n' ~/.config/kitty/kitty.conf
 sed -i 's/font_size 11.0/font_size 20.0/g' ~/.config/kitty/kitty.conf
+sed -i 's/change_font_size all/change_font_size current/g' ~/.config/kitty/kitty.conf
 
 cp -v "$REPO_ROOT/configs/.config/fontconfig/fonts.conf" ~/.config/fontconfig/fonts.conf
 fc-cache -fv
