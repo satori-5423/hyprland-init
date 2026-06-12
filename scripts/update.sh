7b0c934604de60c4d5e7058e02413b7b67e32d09
satori-5423 <suxue2746841150@gmail.com>
2026-06-12 18:08:17 +0800

    kitty: set scrollback_pager in nvim

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,7 +9,7 @@ REPO_ROOT="$(dirname "$SCRIPT_DIR")"
 set -e
 
 # kitty
-sed -i '1i # vim:ft=kitty\nbackground_opacity 0.8\n' ~/.config/kitty/kitty.conf
+sed -i '1i # vim:ft=kitty\nbackground_opacity 0.8\n\n# Neovim\nscrollback_pager nvim --cmd "set eventignore=FileType" +"nnoremap q ZQ" +"call nvim_open_term(0, {})" +"set nomodified nolist" +"$" -\nmap ctrl+shift+h show_scrollback\n' ~/.config/kitty/kitty.conf
 sed -i 's/font_size 11.0/font_size 20.0/g' ~/.config/kitty/kitty.conf
 sed -i 's/change_font_size all/change_font_size current/g' ~/.config/kitty/kitty.conf
 sed -i '/cursor_trail 1/a cursor_trail_decay 0.1 0.42\ncursor_trail_start_threshold 0' ~/.config/kitty/kitty.conf
