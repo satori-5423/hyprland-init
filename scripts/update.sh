09ec05decc6cf1ab89bbcc5319790dfbdc66acbd
satori-5423 <suxue2746841150@gmail.com>
2026-06-12 12:46:42 +0800

    update: we don't need enable_transience

--- scripts/update.sh
+++ scripts/update.sh
@@ -14,6 +14,9 @@ sed -i 's/font_size 11.0/font_size 20.0/g' ~/.config/kitty/kitty.conf
 sed -i 's/change_font_size all/change_font_size current/g' ~/.config/kitty/kitty.conf
 sed -i '/cursor_trail 1/a cursor_trail_decay 0.1 0.42\ncursor_trail_start_threshold 0' ~/.config/kitty/kitty.conf
 
+# fish
+sed -i 's/enable_transience/# enable_transience/g' ~/.config/fish/config.fish
+
 # fontconfig
 cp -v "$REPO_ROOT/configs/.config/fontconfig/fonts.conf" ~/.config/fontconfig/fonts.conf
 fc-cache -fv
