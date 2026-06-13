7bcaa0a302c3ae50539908b9633989501c69ad93
satori-5423 <suxue2746841150@gmail.com>
2026-06-13 10:24:39 +0800

    hypr: set nvim as default text editor

--- configs/.config/hypr/custom/keybinds.lua
+++ configs/.config/hypr/custom/keybinds.lua
@@ -11,16 +11,6 @@ hl.bind("CTRL + SUPER + SHIFT + Down", hl.dsp.window.move({ workspace = "r+5" })
 hl.bind("CTRL + SUPER + SHIFT + Left", hl.dsp.window.move({ workspace = "r-1" }))
 hl.bind("CTRL + SUPER + SHIFT + Right", hl.dsp.window.move({ workspace = "r+1" }))
 
--- Apps
-hl.unbind("SUPER + X")
-hl.bind(
-	"SUPER + X",
-	hl.dsp.exec_cmd(
-		'~/.config/hypr/hyprland/scripts/launch_first_available.sh "zeditor" "command -v nvim && kitty -1 nvim"'
-	),
-	{ description = "App: Text editor" }
-)
-
 -- Lock screen (dispatch to Quickshell's lock handler)
 hl.unbind("SUPER + L")
 hl.bind("SUPER + L", hl.dsp.global("quickshell:lock"), { description = "Misc: Lock" })
--- configs/.config/hypr/custom/variables.lua
+++ configs/.config/hypr/custom/variables.lua
@@ -1,1 +1,5 @@
+-- Custom variables
+
+-- Apps
+textEditor = "~/.config/hypr/hyprland/scripts/launch_first_available.sh 'command -v nvim && kitty -1 fish -c nvim' 'kate' 'emacs'"
 
