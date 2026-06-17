09c4c87677727d660266ea1b901b8556977b124a
satori-5423 <suxue2746841150@gmail.com>
2026-06-18 00:59:27 +0800

    kitty: set symbols nerd font mono

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -11,6 +11,7 @@ set -e
 # kitty
 sed -i '1i # vim:ft=kitty\nbackground_opacity 0.8\n\n# Neovim\nscrollback_pager nvim --cmd "set eventignore=FileType" +"nnoremap q ZQ" +"call nvim_open_term(0, {})" +"set nomodified nolist" +"$" -\nmap ctrl+shift+h show_scrollback\n' ~/.config/kitty/kitty.conf
 sed -i 's/font_size 11.0/font_size 20.0/g' ~/.config/kitty/kitty.conf
+sed -i '/font_size 20.0/a symbol_map U+e000-U+e00a,U+e0a0-U+e0a2,U+e0a3,U+e0b0-U+e0b3,U+e0b4-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ea60-U+ec1e,U+ed00-U+efce,U+f000-U+f2ff,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono' ~/.config/kitty/kitty.conf
 sed -i 's/change_font_size all/change_font_size current/g' ~/.config/kitty/kitty.conf
 sed -i '/cursor_trail 1/a cursor_trail_decay 0.1 0.42\ncursor_trail_start_threshold 0' ~/.config/kitty/kitty.conf
 sed -i '/# Scroll & Zoom/a scrollbar scrolled-and-hovered' ~/.config/kitty/kitty.conf
